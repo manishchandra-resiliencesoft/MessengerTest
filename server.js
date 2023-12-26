5b16dd25ffb5252f768b4d280ee873dc35bce224
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2023-12-26 21:11:14 +0530

    working on webhook

--- server.js
+++ server.js
@@ -8,20 +8,24 @@ const PAGE_ACCESS_TOKEN =
 
 app.use(bodyParser.json());
 
-// URL 1: /webhook
-app.get("/webhook", (req, res) => {
-	let body = req.body;
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", (req, res) => {
+	// Parse the query params
+	let mode = req.query["hub.mode"];
+	let token = req.query["hub.verify_token"];
+	let challenge = req.query["hub.challenge"];
 
-	console.log(`\u{1F7EA} Received webhook:`);
-	console.dir(body, { depth: null });
-	if (body.object === "page") {
-		// Returns a '200 OK' response to all requests
-		res.status(200).send("EVENT_RECEIVED");
-
-		// Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
-	} else {
-		// Return a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
+	// Check if a token and mode is in the query string of the request
+	if (mode && token) {
+		// Check the mode and token sent is correct
+		if (mode === "subscribe" && token === "token") {
+			// Respond with the challenge token from the request
+			console.log("WEBHOOK_VERIFIED");
+			res.status(200).send(challenge);
+		} else {
+			// Respond with '403 Forbidden' if verify tokens do not match
+			res.sendStatus(403);
+		}
 	}
 });
 
