be4fc03d650c1d4a7e277102cde26b8380257563
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2023-12-27 11:51:28 +0530

    working on webhook

--- server.js
+++ server.js
@@ -29,58 +29,9 @@ app.get("/messaging-webhook", (req, res) => {
 	}
 });
 
-app.post("/webhook", (req, res) => {
+app.post("/send-message", (req, res) => {
 	let body = req.body;
 
 	console.log(`\u{1F7EA} Received webhook:`);
 	console.dir(body, { depth: null });
 });
-
-// URL 3: /send-message
-app.post("/send-message", (req, res) => {
-	//   const { recipientId, message } = req.body;
-
-	if (!recipientId || !message) {
-		return res
-			.status(400)
-			.json({ error: "Recipient ID and message are required." });
-	}
-
-	axios
-		.post(
-			"https://graph.facebook.com/v12.0/me/messages",
-			{
-				messaging_type: "RESPONSE",
-				recipient: {
-					id: "122093648912167673",
-				},
-				message: {
-					text: "Hi test message from api",
-				},
-			},
-			{
-				params: {
-					access_token: PAGE_ACCESS_TOKEN,
-				},
-			}
-		)
-		.then((response) => {
-			console.log("Message sent:", response.data);
-			res
-				.status(200)
-				.json({ success: true, message: "Message sent successfully." });
-		})
-		.catch((error) => {
-			console.error("Error sending message:", error.response.data);
-			res.status(500).json({ error: "Error sending message." });
-		});
-});
-
-// URL 4: /test
-app.get("/test", (req, res) => {
-	console.log("GET Request to /test");
-	res.status(200).send("This is a simple GET endpoint for testing.");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
